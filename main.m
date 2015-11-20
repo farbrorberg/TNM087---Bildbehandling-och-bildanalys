2d2d372e4e9a92b4c903999915a5ed098411f27a
Matthias Berg <matbe790@student.liu.se>
2015-11-20 13:41:18 +0100

    Fler bilder och mergeade testfiler

--- main.m
+++ main.m
@@ -1,8 +1,9 @@
 %% Task 1
 
-BasicImageInfo( 'kitten.jpg', 3);
+ BasicImageInfo( 'einstein.jpg', 3)
 
 %% Task 2
 
+SimpleReplace('BoldRedEye.JPG', [200 400 7], [150 300 7], [500, 600, 200, 200] )
 
 %% Task 3
